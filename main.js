b2a5680e3f3ad9572ee26fd93f7b64113766b4aa
ABHAYPEDDI <106728654+ABHAYPEDDI@users.noreply.github.com>
2022-06-11 13:17:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//debug the code to store list
+var updatedimage = [family_book.jpg,Peter_Griffin.jpg,mary_griffin.jpg,stewie_griffin.jpg];
+//debug the code to store list
+var updatedname = [myfamilybook,peter,mary,stewie];
